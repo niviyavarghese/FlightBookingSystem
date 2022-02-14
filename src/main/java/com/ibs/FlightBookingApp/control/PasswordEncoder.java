adb2c34ca459070fe6f53fd300981ed3f88f4cb6
Niviya Varghese <A-10564@ibsplc.com>
2022-02-14 12:41:06 +0530

    first commit

---
+++ src/main/java/com/ibs/FlightBookingApp/control/PasswordEncoder.java
@@ -0,0 +1,9 @@
+package com.ibs.FlightBookingApp;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+	public static void main(String[] args) {
+
+	}
+}
